58685369aff7e13268972f3a5aedc91f11cdc36f
geohub2017 <geohub2017@hotmail.com>
2019-03-10 22:57:00 -0400

    Initial

---
+++ dist/main.js
@@ -0,0 +1,133 @@
+var app;
+require([
+  "esri/Map",
+  "esri/Basemap",
+  "esri/views/MapView",
+  "esri/views/SceneView",
+  "esri/widgets/Search",
+  "esri/core/watchUtils",
+  "dojo/query",
+  // Calcite-maps
+  "calcite-maps/calcitemaps-v0.10",
+  "calcite-maps/calcitemaps-arcgis-support-v0.10",
+  // Bootstrap
+  "bootstrap/Collapse",
+  "bootstrap/Dropdown",
+  "bootstrap/Tab",
+  "dojo/domReady!"
+], function(
+  Map,
+  Basemap,
+  MapView,
+  SceneView,
+  Search,
+  watchUtils,
+  query,
+  CalciteMaps,
+  CalciteMapsArcGIS
+) {
+  // App
+  app = {
+    zoom: 1,
+    center: [-40, 40],
+    basemap: "satellite",
+    viewPadding: {
+      top: 50,
+      bottom: 0
+    },
+    uiPadding: {
+      top: 15,
+      bottom: 15
+    },
+    map: null,
+    mapView: null,
+    sceneView: null,
+    activeView: null,
+    searchWidgetNav: null,
+    containerMap: "mapViewDiv",
+    containerScene: "sceneViewDiv"
+  };
+  // Map
+  app.map = new Map({
+    basemap: app.basemap,
+    ground: "world-elevation"
+  });
+  // 2D View
+  app.mapView = new MapView({
+    container: null, // deactivate
+    map: app.map,
+    zooom: app.zoom,
+    center: app.center,
+    padding: app.viewPadding,
+    ui: {
+      components: ["zoom", "compass", "attribution"],
+      padding: app.uiPadding
+    }
+  });
+  // 3D View
+  app.sceneView = new SceneView({
+    container: app.containerScene, // activate
+    map: app.map,
+    zoom: app.zoom,
+    center: app.center,
+    padding: app.viewPadding,
+    ui: {
+      padding: app.uiPadding
+    }
+  });
+  // Active view is scene
+  setActiveView(app.sceneView);
+  // Create search widget
+  app.searchWidgetNav = new Search({
+    container: "searchNavDiv",
+    view: app.activeView
+  });
+  // Wire-up expand events
+  CalciteMapsArcGIS.setSearchExpandEvents(app.searchWidgetNav);
+  CalciteMapsArcGIS.setPopupPanelSync(app.mapView);
+  CalciteMapsArcGIS.setPopupPanelSync(app.sceneView);
+  // Menu UI - change Basemaps
+  query("#selectBasemapPanel").on("change", function(e) {
+    app.mapView.map.basemap =
+      e.target.options[e.target.selectedIndex].dataset.vector;
+    app.sceneView.map.basemap = e.target.value;
+  });
+  // Tab UI - switch views
+  query(".calcite-navbar li a[data-toggle='tab']").on("click", function(e) {
+    if (e.target.text.indexOf("Map") > -1) {
+      syncViews(app.sceneView, app.mapView);
+      setActiveView(app.mapView);
+    } else {
+      syncViews(app.mapView, app.sceneView);
+      setActiveView(app.sceneView);
+    }
+    syncSearch(app.activeView);
+  });
+  // Views
+  function syncViews(fromView, toView) {
+    var viewPt = fromView.viewpoint.clone();
+    fromView.container = null;
+    if (fromView.type === "3d") {
+      toView.container = app.containerMap;
+    } else {
+      toView.container = app.containerScene;
+    }
+    toView.viewpoint = viewPt;
+    toView.padding = app.viewPadding;
+  }
+  // Search Widget
+  function syncSearch(view) {
+    app.searchWidgetNav.view = view;
+    if (app.searchWidgetNav.selectedResult) {
+      watchUtils.whenTrueOnce(view, "ready", function() {
+        app.searchWidgetNav.autoSelect = false;
+        app.searchWidgetNav.search(app.searchWidgetNav.selectedResult.name);
+        app.searchWidgetNav.autoSelect = true;
+      });
+    }
+  }
+  // Active view
+  function setActiveView(view) {
+    app.activeView = view;
+  }
+});
